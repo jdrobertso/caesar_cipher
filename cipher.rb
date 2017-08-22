beae778e10f62e2c34e3f145864d313c890120a5
Jeremy <jrobertson.88@gmail.com>
2017-08-21 19:09:05 -0500

    Completed the project

--- cipher.rb
+++ cipher.rb
@@ -1,9 +1,9 @@
 puts "What message would you like to encrypt?"
 message = gets.chomp
 puts "How far should I shift it?"
-shift = gets.chomp
-
-message.split
-for each message[] do
-	
-end
+shift = gets.chomp.to_i
+#Takes the message and shifts it the required number of spaces, then prints the message.
+ascii = message.chars.map(&:ord)
+shifted = ascii.map{ |c| c + shift}
+new_message  = shifted.map{|c| c.chr}.join
+puts new_message
